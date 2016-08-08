8e3356c3b1c5a0459fa0ddf87168e49dd119555e
MAURICIO ANTONIO RODRIGUEZ MORALES <mauricio.rodriguez_mora@ucr.ac.cr>
2016-08-08 09:49:43 -0600

    Configuration for database using PostgreSQL

--- sistema_practica/settings.py
+++ sistema_practica/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'sistema_practica.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sistema_practica',
+        'USER': 'sistema_practica',
+        'PASSWORD': 'pr4ct1c4',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
